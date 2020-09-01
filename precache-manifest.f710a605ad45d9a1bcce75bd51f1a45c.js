417ee31a00dfbeffd6fb7621020263675f13b1c6
RusTron <rusan.tron@gmail.com>
2020-09-01 09:44:13 +0300

    Updates

---
+++ precache-manifest.f710a605ad45d9a1bcce75bd51f1a45c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d63291568e88e5f7eba0bf5a79dc59",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5131f936d521ae25616a",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "2c497fa8c70ace53059f",
+    "url": "./static/css/main.f16f4bbc.chunk.css"
+  },
+  {
+    "revision": "5131f936d521ae25616a",
+    "url": "./static/js/2.a151ed51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a151ed51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c497fa8c70ace53059f",
+    "url": "./static/js/main.6675eec0.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.f710a605ad45d9a1bcce75bd51f1a45c.js"
 );
 
 self.addEventListener('message', (event) => {
